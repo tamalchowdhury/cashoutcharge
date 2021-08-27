c874e7ef7ac3da8b34809ee3c3294479670939a2
tamalweb <tamalweb@protonmail.com>
2021-08-27 20:35:59 +0600

    added sitemap, removed gh-pages

---
+++ sitemap-routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route } from "react-router";
+
+export default (
+  <Route>
+    <Route path="/bkash" />
+    <Route path="/nagad" />
+    <Route path="/rocket" />
+    <Route path="/upay" />
+    <Route path="/bkash/5000" />
+    <Route path="/nagad/5000" />
+    <Route path="/rocket/5000" />
+    <Route path="/upay/5000" />
+  </Route>
+);
