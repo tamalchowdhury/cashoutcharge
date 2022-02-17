3a04e2e9a2654ed9e0ec8294cc5cfef10177b4e0
tamalweb <tamalweb@protonmail.com>
2022-02-17 15:11:29 +0600

    added bangla font

--- src/component/Form.js
+++ src/component/Form.js
@@ -99,7 +99,12 @@ export default function Form({ lang, service, value, setValue, switchLang }) {
       </Helmet>
       <div className="form">
         <div className="form__title">
-          <h1>{switchLang("Cashout Calculator", "ক্যাশআউট ক্যালকুলেটর")}</h1>
+          <h1>
+            {switchLang(
+              "Bkash Cashout Calculator",
+              "বিকাশ ক্যাশআউট ক্যালকুলেটর"
+            )}
+          </h1>
         </div>
         <div className="form__body">
           <form onSubmit={handleSubmit} className="">
